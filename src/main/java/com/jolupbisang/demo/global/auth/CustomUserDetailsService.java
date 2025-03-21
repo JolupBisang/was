5ce3dae6aca1ed7e64d241ffda90100e26dc2137
garden-zero <oneyoung0623@gmail.com>
2025-03-22 00:20:10 +0900

    feat: CustomUserDetailsService 구현

---
+++ src/main/java/com/jolupbisang/demo/global/auth/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.jolupbisang.demo.global.auth;
+
+import com.jolupbisang.demo.domain.user.dto.UserInfoRes;
+import com.jolupbisang.demo.domain.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        UserInfoRes user = userService.findById(email);
+        return new CustomUserDetails(
+                user.id(),
+                user.email(),
+                user.nickname()
+        );
+    }
+}
