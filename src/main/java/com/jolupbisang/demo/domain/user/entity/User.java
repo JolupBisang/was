4b605d258bbe5fcc33e0ee94f0a3da66af14aa85
garden-zero <oneyoung0623@gmail.com>
2025-03-21 23:57:34 +0900

    feat: 엔티티 선언

---
+++ src/main/java/com/jolupbisang/demo/domain/user/entity/User.java
@@ -0,0 +1,24 @@
+package com.jolupbisang.demo.domain.user.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "user")
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String nickname;
+}
