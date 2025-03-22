cc60c3c8c5765a975c2c4f5aa8ef01d20d735692
garden-zero <oneyoung0623@gmail.com>
2025-03-23 01:50:39 +0900

    fix: 생성자 주입을 위한 final 키워드 추가

--- src/main/java/com/jolupbisang/demo/domain/user/service/UserService.java
+++ src/main/java/com/jolupbisang/demo/domain/user/service/UserService.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class UserService {
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
     public UserInfoRes findById(String userId) {
         return UserInfoRes.from(userRepository.findById(userId)
