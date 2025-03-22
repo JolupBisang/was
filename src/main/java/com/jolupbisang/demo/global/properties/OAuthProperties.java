045dcdfa840f83eed6e30e6a5469d8f027d88a94
garden-zero <oneyoung0623@gmail.com>
2025-03-23 01:57:52 +0900

    fix: Properties setter 추가

--- src/main/java/com/jolupbisang/demo/global/properties/OAuthProperties.java
+++ src/main/java/com/jolupbisang/demo/global/properties/OAuthProperties.java
@@ -2,10 +2,12 @@ package com.jolupbisang.demo.global.properties;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Getter
+@Setter
 @ConfigurationProperties("oauth2")
 @Component
 public class OAuthProperties {
