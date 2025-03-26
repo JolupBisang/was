985c7dfd033e7e5df295c2dc421c0338cb59e685
garden-zero <oneyoung0623@gmail.com>
2025-03-26 16:08:00 +0900

    feat: Bean Validation시 예외 출력 방식 변경

--- src/main/java/com/jolupbisang/demo/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/jolupbisang/demo/global/exception/GlobalExceptionHandler.java
@@ -2,17 +2,31 @@ package com.jolupbisang.demo.global.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
     @ExceptionHandler(CustomException.class)
     protected ResponseEntity<?> handleCustomException(CustomException ex) {
         return buildErrorResponse(ex, ex.getErrorCode());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<ErrorResponse> handleValidationException(final MethodArgumentNotValidException e) {
+        Map<String, String> errors = new HashMap<>();
+        for (FieldError fieldError : e.getBindingResult().getFieldErrors()) {
+            errors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+        return buildErrorResponse(e, GlobalErrorCode.INVALID_INPUT, errors);
     }
 
     @ExceptionHandler(Exception.class)
@@ -21,13 +35,20 @@ public class GlobalExceptionHandler {
     }
 
     private ResponseEntity<ErrorResponse> buildErrorResponse(Exception ex, ErrorCode errorCode) {
-        String errorId = UUID.randomUUID().toString();
-        log.error("[{}] Exception occurred: {}", errorId, ex.getMessage());
-
-        return ResponseEntity.status(errorCode.getStatus()).body(new ErrorResponse(errorCode.getMessage(), errorId));
+        return buildErrorResponse(ex, errorCode, null);
     }
 
-    private record ErrorResponse(String message, String errorId) {
+    private ResponseEntity<ErrorResponse> buildErrorResponse(Exception ex, ErrorCode errorCode, Map<String, String> errors) {
+        String errorId = UUID.randomUUID().toString();
+        log.error("[{}] Exception occurred: {}", errorId, errorCode.getMessage(), ex);
+
+        return ResponseEntity.status(errorCode.getStatus()).body(new ErrorResponse(errorCode.getMessage(), errorId, errors));
+    }
+
+    private record ErrorResponse(String message, String errorId, Map<String, String> errors) {
+        ErrorResponse(String message, String errorId) {
+            this(message, errorId, null);
+        }
     }
 }
 
