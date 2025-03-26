356a5254bbc1e7554cfa93f5fe454453532d5295
garden-zero <oneyoung0623@gmail.com>
2025-03-26 13:30:31 +0900

    feat: 회의관련 엔티티 추가

---
+++ src/main/java/com/jolupbisang/demo/domain/meeting/Meeting.java
@@ -0,0 +1,41 @@
+package com.jolupbisang.demo.domain.meeting;
+
+import com.jolupbisang.demo.domain.common.BaseTimeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Meeting extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    @Column(name = "meeting_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String location;
+
+    @Column(nullable = false)
+    private LocalDateTime scheduledStartTime;
+
+    @Column(nullable = false)
+    private int targetTime;
+
+    private LocalDateTime actualStartTime;
+
+    private LocalDateTime actualEndTime;
+
+    private String recordUrl;
+}
---
+++ src/main/java/com/jolupbisang/demo/domain/meetingUser/MeetingUser.java
@@ -0,0 +1,32 @@
+package com.jolupbisang.demo.domain.meetingUser;
+
+import com.jolupbisang.demo.domain.meeting.Meeting;
+import com.jolupbisang.demo.domain.user.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class MeetingUser {
+
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    @Column(name = "meeting_user_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "meeting_id")
+    private Meeting meeting;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private boolean isHost;
+
+    @Enumerated(EnumType.STRING)
+    private MeetingUserStatus status;
+}
