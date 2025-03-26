f3aca6489d09f715e22a3ff1772ba7e0039fab37
garden-zero <oneyoung0623@gmail.com>
2025-03-26 16:07:11 +0900

    feat: 필터에서 발생하는 jwt 예외처리 필터 추가

--- src/main/java/com/jolupbisang/demo/global/config/SecurityFilterConfig.java
+++ src/main/java/com/jolupbisang/demo/global/config/SecurityFilterConfig.java
@@ -1,7 +1,9 @@
 package com.jolupbisang.demo.global.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jolupbisang.demo.infrastructure.auth.JwtProvider;
 import com.jolupbisang.demo.presentation.auth.filter.JwtAuthenticationFilter;
+import com.jolupbisang.demo.presentation.auth.filter.JwtExceptionFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,10 +14,16 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 public class SecurityFilterConfig {
     private final UserDetailsService userDetailsService;
     private final JwtProvider jwtProvider;
+    private final ObjectMapper objectMapper;
 
     @Bean
     public JwtAuthenticationFilter jwtAuthenticationFilter() {
         return new JwtAuthenticationFilter(userDetailsService, jwtProvider);
+    }
+
+    @Bean
+    public JwtExceptionFilter jwtExceptionFilter() {
+        return new JwtExceptionFilter(objectMapper);
     }
 }
 
