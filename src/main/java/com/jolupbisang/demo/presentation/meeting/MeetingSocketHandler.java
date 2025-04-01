041c72fca5e42feec299d928bc783abb17b6663f
garden-zero <oneyoung0623@gmail.com>
2025-04-01 22:32:39 +0900

    feat: 오디오 데이터 핸들러 추가

---
+++ src/main/java/com/jolupbisang/demo/presentation/meeting/MeetingSocketHandler.java
@@ -0,0 +1,35 @@
+package com.jolupbisang.demo.presentation.meeting;
+
+
+import com.jolupbisang.demo.application.meeting.service.MeetingService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.BinaryMessage;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.BinaryWebSocketHandler;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class MeetingSocketHandler extends BinaryWebSocketHandler {
+
+    private final MeetingService meetingService;
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        log.info("WebSocket Connection Established: {}", session.getId());
+    }
+
+    @Override
+    protected void handleBinaryMessage(WebSocketSession session, BinaryMessage message) throws Exception {
+        log.info("핸들러 도착");
+        meetingService.processAndSendAudioData(session, message);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        log.info("WebSocket Connection Closed: {} - Status: {}", session.getId(), status);
+    }
+}
