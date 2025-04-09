e5dcaca9d0b60374bbe894af5202d12576e48af7
garden-zero <oneyoung0623@gmail.com>
2025-04-09 16:32:00 +0900

    feat: MeetingSseEmitter 관리 클래스 추가

---
+++ src/main/java/com/jolupbisang/demo/infrastructure/sse/MeetingSseEventType.java
@@ -0,0 +1,8 @@
+package com.jolupbisang.demo.infrastructure.sse;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public enum MeetingSseEventType {
+    SHARE, FEEDBACK, SUMMARY;
+}
---
+++ src/main/java/com/jolupbisang/demo/infrastructure/sse/MeetingSseService.java
@@ -0,0 +1,84 @@
+package com.jolupbisang.demo.infrastructure.sse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class MeetingSseService {
+
+    private final ObjectMapper objectMapper;
+    private final Map<String, Map<String, SseEmitter>> meetingEmitters = new ConcurrentHashMap<>();
+
+    public SseEmitter subscribe(String meetingId, String userId, MeetingSseEventType eventType) {
+        meetingEmitters.putIfAbsent(meetingId, new ConcurrentHashMap<>());
+
+        String emitterKey = userId + "_" + eventType;
+        SseEmitter emitter = createEmitter(meetingId, emitterKey);
+        sendConnectionMessage(meetingId, eventType, emitter);
+        meetingEmitters.get(meetingId).put(emitterKey, emitter);
+
+        return emitter;
+    }
+
+    public void sendEventToMeeting(String meetingId, MeetingSseEventType eventType, Object data) {
+        if (!meetingEmitters.containsKey(meetingId)) {
+            log.error("연결된 클라이언트가 없습니다. meetingId: {}", meetingId);
+            return;
+        }
+
+        Map<String, SseEmitter> emitters = meetingEmitters.get(meetingId);
+        emitters.forEach((key, emitter) -> {
+            if (key.endsWith("_" + eventType)) {
+                try {
+                    emitter.send(SseEmitter.event()
+                            .name(eventType.toString())
+                            .data(objectMapper.writeValueAsString(data)));
+                } catch (IOException e) {
+                    emitter.completeWithError(e);
+                }
+            }
+        });
+    }
+
+    private SseEmitter createEmitter(String meetingId, String emitterKey) {
+        SseEmitter emitter = new SseEmitter(180000L);
+        emitter.onCompletion(() -> {
+            meetingEmitters.get(meetingId).remove(emitterKey);
+            if (meetingEmitters.get(meetingId).isEmpty()) {
+                meetingEmitters.remove(meetingId);
+            }
+        });
+
+        emitter.onTimeout(() -> {
+            log.info("[{}]: meetingId {} SSE connection timed out", emitter, meetingId);
+            emitter.complete();
+        });
+
+        emitter.onError((ex) -> {
+            log.info("[{}]: meetingId {} SSE connection error", emitter, meetingId, ex);
+            emitter.complete();
+        });
+
+        return emitter;
+    }
+
+    private void sendConnectionMessage(String meetingId, MeetingSseEventType eventType, SseEmitter emitter) {
+        try {
+            emitter.send(SseEmitter.event()
+                    .name("connect")
+                    .data("connected to " + eventType + " event stream"));
+        } catch (IOException e) {
+            log.info("[{}]: meetingId {} SSE IOException error", emitter, meetingId, e);
+            emitter.completeWithError(e);
+        }
+    }
+}
