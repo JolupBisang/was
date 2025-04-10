430fa4063f8f7ba1ea46e728ac72922fe3e94913
garden-zero <oneyoung0623@gmail.com>
2025-04-10 22:39:30 +0900

    feat: 회의 존재 및 참여자 검증 클래스 분리

--- src/main/java/com/jolupbisang/demo/application/meeting/exception/MeetingAccessErrorCode.java
+++ src/main/java/com/jolupbisang/demo/application/meeting/exception/MeetingAccessErrorCode.java
@@ -7,8 +7,10 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 @RequiredArgsConstructor
-public enum MeetingErrorCode implements ErrorCode {
-    NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 회원입니다.");
+public enum MeetingAccessErrorCode implements ErrorCode {
+    NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 회의입니다."),
+    NOT_IN_PROGRESS(HttpStatus.BAD_REQUEST, "진행중인 회의가 아닙니다."),
+    NOT_PARTICIPANT(HttpStatus.FORBIDDEN, "해당 회의의 참여자가 아닙니다.");
 
     private final HttpStatus status;
     private final String message;
