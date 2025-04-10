62987420e8b52acd43388ba41d7faf63d251fbb2
garden-zero <oneyoung0623@gmail.com>
2025-04-10 22:40:53 +0900

    feat: 테스트 메세지 전송 엔드포인트 추가

--- src/main/java/com/jolupbisang/demo/presentation/participationRate/ParticipationRateController.java
+++ src/main/java/com/jolupbisang/demo/presentation/participationRate/ParticipationRateController.java
@@ -5,10 +5,7 @@ import com.jolupbisang.demo.infrastructure.auth.security.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @RestController
@@ -23,5 +20,10 @@ public class ParticipationRateController {
                                 @AuthenticationPrincipal CustomUserDetails userDetails) {
 
         return participationRateService.subscribe(meetingId, userDetails.getUserId());
+    }
+
+    @PostMapping("/send/{meetingId}")
+    public void sendTestSummary(@PathVariable Long meetingId) {
+        participationRateService.sendTestMessage(meetingId);
     }
 }
