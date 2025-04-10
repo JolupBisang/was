5fa00791693f87919f779e27cbe91f1551912568
garden-zero <oneyoung0623@gmail.com>
2025-04-10 17:54:21 +0900

    feat: 회의가 진행중이지 않을 때 예외를 반환하도록 변경

--- src/main/java/com/jolupbisang/demo/application/meeting/exception/MeetingErrorCode.java
+++ src/main/java/com/jolupbisang/demo/application/meeting/exception/MeetingErrorCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum MeetingErrorCode implements ErrorCode {
-    NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 회원입니다.");
+    NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 회원입니다."),
+    NOT_IN_PROGRESS(HttpStatus.BAD_REQUEST, "진행중인 회의가 아닙니다.");
 
     private final HttpStatus status;
     private final String message;
--- src/main/java/com/jolupbisang/demo/application/participationRate/ParticipationRateService.java
+++ src/main/java/com/jolupbisang/demo/application/participationRate/ParticipationRateService.java
@@ -1,5 +1,10 @@
 package com.jolupbisang.demo.application.participationRate;
 
+import com.jolupbisang.demo.application.meeting.exception.MeetingErrorCode;
+import com.jolupbisang.demo.domain.meeting.Meeting;
+import com.jolupbisang.demo.domain.meeting.MeetingStatus;
+import com.jolupbisang.demo.global.exception.CustomException;
+import com.jolupbisang.demo.infrastructure.meeting.MeetingRepository;
 import com.jolupbisang.demo.infrastructure.sse.MeetingSseEventType;
 import com.jolupbisang.demo.infrastructure.sse.MeetingSseService;
 import lombok.RequiredArgsConstructor;
@@ -12,9 +17,17 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 @RequiredArgsConstructor
 public class ParticipationRateService {
 
+    private final MeetingRepository meetingRepository;
     private final MeetingSseService meetingSseService;
 
     public SseEmitter subscribe(Long meetingId, Long userId) {
+        Meeting meeting = meetingRepository.findById(meetingId)
+                .orElseThrow(() -> new CustomException(MeetingErrorCode.NOT_FOUND));
+
+        if (!meeting.getMeetingStatus().equals(MeetingStatus.IN_PROGRESS)) {
+            throw new CustomException(MeetingErrorCode.NOT_IN_PROGRESS);
+        }
+
         return meetingSseService.subscribe(String.valueOf(meetingId), String.valueOf(userId), MeetingSseEventType.PARTICIPATION_RATE);
     }
 
