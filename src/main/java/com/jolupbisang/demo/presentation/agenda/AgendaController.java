bcdcaf04a4be622a7a5d16c86eaf327aeaa6a06f
garden-zero <oneyoung0623@gmail.com>
2025-04-16 18:43:14 +0900

    feat: Agenda 관련 controller 와 service dto 분리

--- src/main/java/com/jolupbisang/demo/presentation/agenda/AgendaController.java
+++ src/main/java/com/jolupbisang/demo/presentation/agenda/AgendaController.java
@@ -1,9 +1,12 @@
 package com.jolupbisang.demo.presentation.agenda;
 
 import com.jolupbisang.demo.application.agenda.service.AgendaService;
+import com.jolupbisang.demo.global.response.SuccessResponse;
 import com.jolupbisang.demo.infrastructure.auth.security.CustomUserDetails;
-import com.jolupbisang.demo.presentation.agenda.dto.AgendaDetailRes;
-import com.jolupbisang.demo.presentation.agenda.dto.AgendaStatusReq;
+import com.jolupbisang.demo.presentation.agenda.api.AgendaControllerApi;
+import com.jolupbisang.demo.presentation.agenda.dto.request.AgendaStatusReq;
+import com.jolupbisang.demo.presentation.agenda.dto.response.AgendaChangeStatusRes;
+import com.jolupbisang.demo.presentation.agenda.dto.response.AgendaDetailRes;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -14,26 +17,27 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/agendas")
-public class AgendaController {
+public class AgendaController implements AgendaControllerApi {
     private final AgendaService agendaService;
 
+    @Override
     @PatchMapping("/{agendaId}")
     public ResponseEntity<?> changeAgendaStatus(@RequestBody @Valid AgendaStatusReq agendaStatusReq,
                                                 @PathVariable("agendaId") Long agendaId,
                                                 @AuthenticationPrincipal CustomUserDetails customUserDetails) {
-        agendaService.changeAgendaStatus(
-                agendaId,
-                customUserDetails.getUserId(),
-                agendaStatusReq.isCompleted());
 
-        return ResponseEntity.status(HttpStatus.ACCEPTED).body("회의 안건 상태 변경 성공");
+        AgendaChangeStatusRes response = AgendaChangeStatusRes.of(agendaService.changeAgendaStatus(agendaId, customUserDetails.getUserId(), agendaStatusReq.isCompleted()));
+
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(SuccessResponse.of("회의 안건 상태변경 성공", response));
     }
 
+    @Override
     @GetMapping("/{meetingId}")
     public ResponseEntity<?> getAgendas(@PathVariable("meetingId") Long meetingId,
                                         @AuthenticationPrincipal CustomUserDetails customUserDetails) {
 
-        return ResponseEntity.status(HttpStatus.OK).body(
-                AgendaDetailRes.fromDto(agendaService.findByMeetingId(meetingId, customUserDetails.getUserId())));
+        AgendaDetailRes agendaDetailRes = AgendaDetailRes.fromDto(agendaService.findByMeetingId(meetingId, customUserDetails.getUserId()));
+
+        return ResponseEntity.status(HttpStatus.OK).body(SuccessResponse.of(("회의 안건 조회 성공"), agendaDetailRes));
     }
 }
