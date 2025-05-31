759619bba98bdf623b59a6d76b5b191995662da9
garden-zero <oneyoung0623@gmail.com>
2025-05-31 15:32:44 +0900

    feat: 피드백, 중간요약, 전체요약 엔티티 추가

---
+++ src/main/java/com/jolupbisang/demo/domain/recap/Recap.java
@@ -0,0 +1,23 @@
+package com.jolupbisang.demo.domain.recap;
+
+import com.jolupbisang.demo.domain.meeting.Meeting;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Recap {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "meeting_id")
+    private Meeting meeting;
+
+    private String content;
+}
---
+++ src/main/java/com/jolupbisang/demo/domain/summary/Summary.java
@@ -0,0 +1,23 @@
+package com.jolupbisang.demo.domain.summary;
+
+import com.jolupbisang.demo.domain.meeting.Meeting;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Summary {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "meeting_id")
+    private Meeting meeting;
+
+    private String content;
+}
