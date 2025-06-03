b19c03967dc11e90bd67fd14879c729f7944a2c0
garden-zero <oneyoung0623@gmail.com>
2025-06-03 17:59:30 +0900

    feat: 임베드 벡터 조회기능 추가

--- src/main/java/com/jolupbisang/demo/infrastructure/audio/EmbeddedVectorRepository.java
+++ src/main/java/com/jolupbisang/demo/infrastructure/audio/EmbeddedVectorRepository.java
@@ -1,8 +1,11 @@
 package com.jolupbisang.demo.infrastructure.audio;
 
 import java.io.IOException;
+import java.util.List;
 
 public interface EmbeddedVectorRepository {
 
     void save(long userId, byte[] audio) throws IOException;
+
+    List<byte[]> findAllByUserId(long userId);
 }
