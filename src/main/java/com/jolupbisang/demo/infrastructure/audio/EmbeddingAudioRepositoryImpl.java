f33c5c27ab6695aafc4ca1d37a8701093c413290
garden-zero <oneyoung0623@gmail.com>
2025-06-03 13:12:52 +0900

    feat: 임베드 벡터로 변환되기 전 오디오 데이터 저장 레포지토리 추가

---
+++ src/main/java/com/jolupbisang/demo/infrastructure/audio/EmbeddingAudioRepositoryImpl.java
@@ -0,0 +1,35 @@
+package com.jolupbisang.demo.infrastructure.audio;
+
+import com.jolupbisang.demo.infrastructure.aws.s3.S3ClientUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+@Slf4j
+@RequiredArgsConstructor
+@Repository
+public class EmbeddingAudioRepositoryImpl implements EmbeddingAudioRepository {
+
+    private final S3ClientUtil s3ClientUtil;
+
+    public void save(long userId, byte[] audio) throws IOException {
+        String s3Key = generateS3Key(userId);
+        String contentType = "application/octet-stream";
+
+        try (InputStream audioInputStream = new ByteArrayInputStream(audio)) {
+            s3ClientUtil.uploadInputStream(s3Key, audioInputStream, audio.length, contentType);
+        } catch (IOException e) {
+            log.error("Failed to save audio embedding {} to S3 for userId: {}.",
+                    s3Key, userId, e);
+            throw e;
+        }
+    }
+
+    private String generateS3Key(long userId) {
+        return String.format("embedding/user-%d/%s", userId, System.currentTimeMillis());
+    }
+}
