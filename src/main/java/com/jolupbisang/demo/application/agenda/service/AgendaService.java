5abaa520752043cb22c8855ce669486b3ee3a67f
garden-zero <oneyoung0623@gmail.com>
2025-06-05 15:20:47 +0900

    feat: 아젠다 추가 및 삭제 기능 추가

--- src/main/java/com/jolupbisang/demo/application/agenda/service/AgendaService.java
+++ src/main/java/com/jolupbisang/demo/application/agenda/service/AgendaService.java
@@ -4,8 +4,10 @@ import com.jolupbisang.demo.application.agenda.dto.AgendaDetail;
 import com.jolupbisang.demo.application.agenda.exception.AgendaErrorCode;
 import com.jolupbisang.demo.application.common.MeetingAccessValidator;
 import com.jolupbisang.demo.domain.agenda.Agenda;
+import com.jolupbisang.demo.domain.meeting.Meeting;
 import com.jolupbisang.demo.global.exception.CustomException;
 import com.jolupbisang.demo.infrastructure.agenda.AgendaRepository;
+import com.jolupbisang.demo.infrastructure.meeting.MeetingRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,6 +18,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class AgendaService {
     private final AgendaRepository agendaRepository;
+    private final MeetingRepository meetingRepository;
     private final MeetingAccessValidator meetingAccessValidator;
 
     @Transactional
@@ -36,5 +39,37 @@ public class AgendaService {
         return agendas.stream()
                 .map(AgendaDetail::fromEntity)
                 .toList();
+    }
+
+    @Transactional
+    public long addAgenda(Long meetingId, Long userId, String content) {
+        meetingAccessValidator.validateUserIsHost(meetingId, userId);
+
+        Meeting meeting = meetingRepository.findById(meetingId)
+                .orElseThrow(() -> new CustomException(AgendaErrorCode.MEETING_NOT_FOUND));
+
+        if (meeting.isCompleted() || meeting.isCancelled()) {
+            throw new CustomException(AgendaErrorCode.CANNOT_ADD_AGENDA);
+        }
+
+        Agenda agenda = new Agenda(meeting, content);
+        agendaRepository.save(agenda);
+
+        return agenda.getId();
+    }
+
+    @Transactional
+    public void deleteAgenda(Long agendaId, Long userId) {
+        Agenda agenda = agendaRepository.findById(agendaId)
+                .orElseThrow(() -> new CustomException(AgendaErrorCode.NOT_FOUND));
+        Meeting meeting = agenda.getMeeting();
+
+        meetingAccessValidator.validateUserIsHost(meeting.getId(), userId);
+
+        if (!meeting.isWaiting()) {
+            throw new CustomException(AgendaErrorCode.CANNOT_DELETE_AGENDA);
+        }
+
+        agendaRepository.delete(agenda);
     }
 }
