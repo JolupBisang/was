3467211284b09a02c2b462d8a28683342df7f2c0
garden-zero <oneyoung0623@gmail.com>
2025-06-06 21:19:20 +0900

    docs: 본인 정보 조회 기능 Swagger 작성

--- src/main/java/com/jolupbisang/demo/presentation/user/api/UserControllerApi.java
+++ src/main/java/com/jolupbisang/demo/presentation/user/api/UserControllerApi.java
@@ -1,5 +1,6 @@
 package com.jolupbisang.demo.presentation.user.api;
 
+import com.jolupbisang.demo.infrastructure.auth.security.CustomUserDetails;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -8,6 +9,8 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Tag(name = "사용자", description = "사용자 관련 API")
@@ -44,4 +47,36 @@ public interface UserControllerApi {
     ResponseEntity<?> getUserInfo(
             @Parameter(description = "사용자 이메일", required = true, example = "user@example.com")
             @PathVariable("email") String email);
+
+
+    @Operation(summary = "본인 정보 조회", description = "본인 정보를 조회합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "조회 성공",
+                    content = @Content(mediaType = "application/json", examples = {
+                            @ExampleObject(name = "조회 성공", value = """
+                                        {
+                                            "message": "회원 조회 성공",
+                                            "data": {
+                                                "id": 200001,
+                                                "email": "oneyoung0623@gmail.com",
+                                                "nickname": "정원영"
+                                            }
+                                        }
+                                    """),
+                    })
+            ),
+            @ApiResponse(responseCode = "404", description = "사용자를 찾을 수 없음 - 일어나지 않지만 가능성 있음",
+                    content = @Content(mediaType = "application/json", examples = {
+                            @ExampleObject(name = "조회 실패", value = """
+                                        {
+                                            "message": "존재하지 않는 회원입니다.",
+                                            "errorId": "bac4f73e-07e8-41b3-86cb-075afa787bee",
+                                            "errors": null
+                                        }
+                                    """),
+                    })
+            ),
+    })
+    @GetMapping("/my-profile")
+    ResponseEntity<?> getMyProfile(@AuthenticationPrincipal CustomUserDetails userDetails);
 }
