7a7bc3d468ffa279c0e1599e87876a6ece9dda9c
garden-zero <oneyoung0623@gmail.com>
2025-06-06 15:46:15 +0900

    feat: feedback 조회 기능 추가

--- src/main/java/com/jolupbisang/demo/infrastructure/feedback/FeedbackRepository.java
+++ src/main/java/com/jolupbisang/demo/infrastructure/feedback/FeedbackRepository.java
@@ -1,7 +1,11 @@
 package com.jolupbisang.demo.infrastructure.feedback;
 
 import com.jolupbisang.demo.domain.feedback.Feedback;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
+    
+    Slice<Feedback> findByMeetingId(Long meetingId, Pageable pageable);
 }
