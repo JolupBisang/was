03c063350e1a3886eecd52ec11521498a672b5c7
garden-zero <oneyoung0623@gmail.com>
2025-06-07 21:40:58 +0900

    feat: Presigned URL을 생성하기 위한 설정 추가

--- src/main/java/com/jolupbisang/demo/global/config/AWSConfig.java
+++ src/main/java/com/jolupbisang/demo/global/config/AWSConfig.java
@@ -9,6 +9,7 @@ import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.http.apache.ApacheHttpClient;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 import software.amazon.awssdk.services.sfn.SfnClient;
 
 import java.time.Duration;
@@ -44,6 +45,19 @@ public class AWSConfig {
         AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
 
         return S3Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(credentials))
+                .build();
+    }
+
+    @Bean
+    public S3Presigner s3Presigner() {
+        String region = awsProperties.getRegion();
+        String accessKey = awsProperties.getCredentials().getAccessKey();
+        String secretKey = awsProperties.getCredentials().getSecretKey();
+        AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
+
+        return S3Presigner.builder()
                 .region(Region.of(region))
                 .credentialsProvider(StaticCredentialsProvider.create(credentials))
                 .build();
