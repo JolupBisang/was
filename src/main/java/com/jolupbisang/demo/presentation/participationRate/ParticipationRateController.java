4272f1247c85391d56fb1de5f3c9493e9e9592b4
garden-zero <oneyoung0623@gmail.com>
2025-06-08 20:14:43 +0900

    fix: nginx 문제로 sse 경로 재설정

--- src/main/java/com/jolupbisang/demo/presentation/participationRate/ParticipationRateController.java
+++ src/main/java/com/jolupbisang/demo/presentation/participationRate/ParticipationRateController.java
@@ -5,13 +5,11 @@ import com.jolupbisang.demo.application.participationRate.dto.ParticipationRateH
 import com.jolupbisang.demo.infrastructure.auth.security.CustomUserDetails;
 import com.jolupbisang.demo.presentation.participationRate.api.ParticipationRateControllerApi;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,14 +17,6 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 public class ParticipationRateController implements ParticipationRateControllerApi {
 
     private final ParticipationRateService participationRateService;
-
-    @Override
-    @GetMapping(path = "/subscribe/{meetingId}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public SseEmitter subscribe(@PathVariable(value = "meetingId") Long meetingId,
-                                @AuthenticationPrincipal CustomUserDetails userDetails) {
-
-        return participationRateService.subscribe(meetingId, userDetails.getUserId());
-    }
 
     @Override
     @GetMapping("/{meetingId}")
--- src/main/java/com/jolupbisang/demo/presentation/summary/SummaryController.java
+++ src/main/java/com/jolupbisang/demo/presentation/summary/SummaryController.java
@@ -9,14 +9,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,18 +19,11 @@ public class SummaryController implements SummaryControllerApi {
 
     private final SummaryService summaryService;
 
-    @GetMapping(path = "/subscribe/{meetingId}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public SseEmitter subscribe(@PathVariable Long meetingId,
-                                @AuthenticationPrincipal CustomUserDetails userDetails) {
-
-        return summaryService.subscribe(meetingId, userDetails.getUserId());
-    }
-
     @GetMapping("/{meetingId}")
     public Slice<SummaryListRes> getSummaries(@PathVariable Long meetingId,
-                                           @RequestParam(defaultValue = "false") Boolean isRecap,
-                                           @PageableDefault(size = 30, sort = "timestamp", direction = Sort.Direction.DESC) Pageable pageable,
-                                           @AuthenticationPrincipal CustomUserDetails userDetails) {
+                                              @RequestParam(defaultValue = "false") Boolean isRecap,
+                                              @PageableDefault(size = 30, sort = "timestamp", direction = Sort.Direction.DESC) Pageable pageable,
+                                              @AuthenticationPrincipal CustomUserDetails userDetails) {
 
         return summaryService.getSummaries(meetingId, userDetails.getUserId(), isRecap, pageable);
     }
