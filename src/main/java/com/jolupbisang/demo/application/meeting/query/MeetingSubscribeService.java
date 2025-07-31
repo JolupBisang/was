3cab5d3afd25f552ba566407acf58f44f2bec12f
garden-zero <oneyoung0623@gmail.com>
2025-07-31 17:33:37 +0900

    feat: SSE 기능 확장 및 도메인 상관없이 이용하도록 개선

---
+++ src/main/java/com/jolupbisang/demo/application/meeting/query/MeetingSubscribeService.java
@@ -0,0 +1,29 @@
+package com.jolupbisang.demo.application.meeting.query;
+
+import com.jolupbisang.demo.application.meeting.exception.MeetingNotFoundException;
+import com.jolupbisang.demo.domain.meeting.exception.NotParticipantException;
+import com.jolupbisang.demo.domain.meeting.model.Meeting;
+import com.jolupbisang.demo.infrastructure.meeting.MeetingRepository;
+import com.jolupbisang.demo.infrastructure.sse.MeetingSseManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+@Service
+@RequiredArgsConstructor
+public class MeetingSubscribeService {
+
+    private final MeetingRepository meetingRepository;
+    private final MeetingSseManager meetingSseManager;
+
+    public SseEmitter subscribe(Long meetingId, Long userId) {
+        Meeting meeting = meetingRepository.findByIdWithParticipant(meetingId)
+                .orElseThrow(MeetingNotFoundException::new);
+
+        if (!meeting.isParticipant(userId)) {
+            throw new NotParticipantException();
+        }
+
+        return meetingSseManager.subscribe(meetingId, userId);
+    }
+}
