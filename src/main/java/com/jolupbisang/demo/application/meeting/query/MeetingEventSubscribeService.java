f829a8b500ee718dc82f54ce0353f29e18e13d59
garden-zero <oneyoung0623@gmail.com>
2025-08-02 00:32:57 +0900

    feat: sse 구독 엔드포인트 통합

--- src/main/java/com/jolupbisang/demo/application/meeting/query/MeetingEventSubscribeService.java
+++ src/main/java/com/jolupbisang/demo/application/meeting/query/MeetingEventSubscribeService.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @Service
 @RequiredArgsConstructor
-public class MeetingSubscribeService {
+public class MeetingEventSubscribeService {
 
     private final MeetingRepository meetingRepository;
     private final MeetingSseManager meetingSseManager;
---
+++ src/main/java/com/jolupbisang/demo/presentation/meeting/MeetingEventSubscribeController.java
@@ -0,0 +1,25 @@
+package com.jolupbisang.demo.presentation.meeting;
+
+import com.jolupbisang.demo.application.meeting.query.MeetingEventSubscribeService;
+import com.jolupbisang.demo.infrastructure.auth.security.CustomUserDetails;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+@RestController
+@RequiredArgsConstructor
+public class MeetingEventSubscribeController {
+
+    private final MeetingEventSubscribeService meetingEventSubscribeService;
+
+    @GetMapping(path = "/api/meetings/{meetingId}/events/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public SseEmitter subscribe(@PathVariable Long meetingId,
+                                @AuthenticationPrincipal CustomUserDetails userDetails) {
+
+        return meetingEventSubscribeService.subscribe(meetingId, userDetails.getUserId());
+    }
+}
