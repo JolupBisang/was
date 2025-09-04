88e1202fb93cf79da2946b228181c8f3917606de
garden-zero <oneyoung0623@gmail.com>
2025-09-04 20:51:29 +0900

    refactor: 실시간 참여율 event 의존성 변경

--- src/main/java/com/jolupbisang/demo/application/meeting/command/LiveParticipationRateUpdateService.java
+++ src/main/java/com/jolupbisang/demo/application/meeting/command/LiveParticipationRateUpdateService.java
@@ -1,7 +1,8 @@
 package com.jolupbisang.demo.application.meeting.command;
 
-import com.jolupbisang.demo.application.event.whisper.WhisperDiarizedEvent;
-import com.jolupbisang.demo.infrastructure.audio.client.dto.response.DiarizedResponse;
+import com.jolupbisang.demo.application.segment.event.CompletedSegmentReceivedEvent;
+import com.jolupbisang.demo.application.segment.event.SegmentDto;
+import com.jolupbisang.demo.application.segment.event.WordDto;
 import com.jolupbisang.demo.infrastructure.participationRate.RealTimeParticipationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
@@ -14,22 +15,20 @@ public class LiveParticipationRateUpdateService {
     private final RealTimeParticipationRepository realTimeParticipationRepository;
 
     @EventListener
-    public void addToParticipationData(WhisperDiarizedEvent event) {
-        DiarizedResponse diarizedResponse = event.getDiarizedResponse();
-        long groupId = diarizedResponse.groupId();
-
-        for (DiarizedResponse.Segment segment : diarizedResponse.completed()) {
-            processSegment(groupId, segment);
+    public void addToParticipationData(CompletedSegmentReceivedEvent event) {
+        for (SegmentDto segmentDto : event.completed()) {
+            processSegment(event.meetingId(), segmentDto);
         }
     }
 
-    private void processSegment(long meetingId, DiarizedResponse.Segment segment) {
+    private void processSegment(long meetingId, SegmentDto segment) {
         if (segment.words() == null || segment.words().isEmpty()) {
             return;
         }
 
-        DiarizedResponse.Word firstWord = segment.words().get(0);
-        DiarizedResponse.Word lastWord = segment.words().get(segment.words().size() - 1);
+        WordDto firstWord = segment.words().get(0);
+        WordDto lastWord = segment.words().get(segment.words().size() - 1);
+
 
         long participationDuration = lastWord.end() - firstWord.start();
         realTimeParticipationRepository.increaseParticipation(meetingId, segment.userId(), participationDuration);
