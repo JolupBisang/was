2710ceec40a38ac71cb9a5f8f098b5f8680053b1
garden-zero <oneyoung0623@gmail.com>
2025-11-08 17:49:53 +0900

    feat: 회의 종료시 종료메세지 전송 및 완전 종료 시 세션 종료 기능 추가

--- src/main/java/com/jolupbisang/demo/application/meeting/Intergration/MeetingWebsocketRemovalService.java
+++ src/main/java/com/jolupbisang/demo/application/meeting/Intergration/MeetingWebsocketRemovalService.java
@@ -1,9 +1,13 @@
 package com.jolupbisang.demo.application.meeting.Intergration;
 
 import com.jolupbisang.demo.application.meeting.event.MeetingSessionClosedEvent;
+import com.jolupbisang.demo.domain.meeting.event.MeetingCompletedEvent;
+import com.jolupbisang.demo.domain.meeting.model.MeetingCompletedOrder;
 import com.jolupbisang.demo.infrastructure.meeting.MeetingWebsocketManager;
+import com.jolupbisang.demo.presentation.audio.dto.response.SocketResponseType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,5 +22,18 @@ public class MeetingWebsocketRemovalService {
                 event.meetingId(),
                 event.userId()
         );
+    }
+
+    @Order(MeetingCompletedOrder.COMPLETED_EVENT_SENDING)
+    @EventListener
+    public void sendCompletedEvent(MeetingCompletedEvent event) {
+        websocketManager.sendToMeeting(event.meetingId(), SocketResponseType.COMPLETION_SCHEDULED, "회의가 종료되었습니다. 추가작업을 진행합니다.");
+    }
+
+    @Order(MeetingCompletedOrder.CLEAN_UP_SESSION)
+    @EventListener
+    public void cleanUpSession(MeetingCompletedEvent event) {
+        websocketManager.sendToMeeting(event.meetingId(), SocketResponseType.MEETING_COMPLETED, "회의가 종료되었습니다.");
+        websocketManager.closeAllSessionOfMeeting(event.meetingId());
     }
 }
--- src/main/java/com/jolupbisang/demo/domain/meeting/model/MeetingCompletedOrder.java
+++ src/main/java/com/jolupbisang/demo/domain/meeting/model/MeetingCompletedOrder.java
@@ -5,9 +5,11 @@ public final class MeetingCompletedOrder {
     private MeetingCompletedOrder() {
     }
 
-    public static final int CONTEXT_SCHEDULING_TERMINATION = 1;
-    public static final int LIVE_PARTICIPATION_SCHEDULING_TERMINATION = 2;
-    public static final int LIVE_PARTICIPATION_SAVING = 3;
-    public static final int RECAP_SUMMARY_CREATION = 4;
-    public static final int AUDIO_MERGING = 5;
+    public static final int COMPLETED_EVENT_SENDING = 1;
+    public static final int CONTEXT_SCHEDULING_TERMINATION = 2;
+    public static final int LIVE_PARTICIPATION_SCHEDULING_TERMINATION = 3;
+    public static final int LIVE_PARTICIPATION_SAVING = 4;
+    public static final int RECAP_SUMMARY_CREATION = 5;
+    public static final int AUDIO_MERGING = 6;
+    public static final int CLEAN_UP_SESSION = 7;
 }
