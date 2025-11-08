578cf46da869ab40164438218211aa4314be0c28
garden-zero <oneyoung0623@gmail.com>
2025-11-08 22:08:54 +0900

    fix: 참여자 추가시 fetchJoin으로 들고오도록 변경

--- src/main/java/com/jolupbisang/demo/application/meeting/command/ParticipantAdditionService.java
+++ src/main/java/com/jolupbisang/demo/application/meeting/command/ParticipantAdditionService.java
@@ -1,16 +1,17 @@
 package com.jolupbisang.demo.application.meeting.command;
 
 import com.jolupbisang.demo.application.meeting.command.dto.ParticipantAdditionRes;
-import com.jolupbisang.demo.global.exception.NotFoundException;
 import com.jolupbisang.demo.domain.meeting.dto.ParticipantAddReq;
 import com.jolupbisang.demo.domain.meeting.model.Meeting;
 import com.jolupbisang.demo.domain.meeting.model.MeetingRole;
 import com.jolupbisang.demo.domain.meeting.model.ParticipantDetail;
 import com.jolupbisang.demo.domain.user.User;
+import com.jolupbisang.demo.global.exception.NotFoundException;
 import com.jolupbisang.demo.infrastructure.meeting.MeetingRepository;
 import com.jolupbisang.demo.infrastructure.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -21,8 +22,9 @@ public class ParticipantAdditionService {
     private final MeetingRepository meetingRepository;
     private final UserRepository userRepository;
 
+    @Transactional
     public ParticipantAdditionRes addParticipants(long meetingId, long accessUserId, ParticipantAddReq participantAddReq) {
-        Meeting meeting = meetingRepository.findById(meetingId)
+        Meeting meeting = meetingRepository.findByIdWithParticipant(meetingId)
                 .orElseThrow(() -> new NotFoundException("meetingId: %d", meetingId));
 
         List<ParticipantDetail> participantDetails = createParticipantDetailByEmail(participantAddReq.emails());
