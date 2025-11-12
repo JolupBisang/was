3dda9698760db0fd00f4679a261968aefad88cd1
garden-zero <oneyoung0623@gmail.com>
2025-11-12 17:38:35 +0900

    feat: 팀 목록 조회 기능 추가

--- src/main/java/com/jolupbisang/demo/infrastructure/team/TeamRepository.java
+++ src/main/java/com/jolupbisang/demo/infrastructure/team/TeamRepository.java
@@ -3,6 +3,9 @@ package com.jolupbisang.demo.infrastructure.team;
 import com.jolupbisang.demo.domain.team.model.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TeamRepository extends JpaRepository<Team, Long> {
     
+    List<Team> findByMembersUserId(Long userId);
 }
