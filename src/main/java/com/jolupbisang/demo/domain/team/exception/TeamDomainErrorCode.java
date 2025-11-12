4968d347469bc04cf0283c4a710f97a49bc74829
garden-zero <oneyoung0623@gmail.com>
2025-11-12 14:16:20 +0900

    feat: 팀 예외 코드 추가

---
+++ src/main/java/com/jolupbisang/demo/domain/team/exception/TeamDomainErrorCode.java
@@ -0,0 +1,16 @@
+package com.jolupbisang.demo.domain.team.exception;
+
+import com.jolupbisang.demo.global.exception.ErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum TeamDomainErrorCode implements ErrorCode {
+    INVALID_TEAM_NAME("TD-0001", HttpStatus.BAD_REQUEST, "잘못된 팀 이름입니다.");
+
+    private final String code;
+    private final HttpStatus status;
+    private final String message;
+}
