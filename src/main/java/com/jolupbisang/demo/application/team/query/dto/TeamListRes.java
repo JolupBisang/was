590cce1f86d9f71e580c71dd19afb4e2d1705668
정원영 <85011588+gardenzeeero@users.noreply.github.com>
2025-11-13 17:54:19 +0900

    feat: 팀 조회시 배열이 아니라 객체로 전달 (#71)

--- src/main/java/com/jolupbisang/demo/application/team/query/dto/TeamListRes.java
+++ src/main/java/com/jolupbisang/demo/application/team/query/dto/TeamListRes.java
@@ -4,31 +4,40 @@ import com.jolupbisang.demo.domain.meeting.model.Meeting;
 import com.jolupbisang.demo.domain.team.model.Team;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public record TeamListRes(
-        Long teamId,
-        String teamName,
-        String meetingName,
-        LocalDateTime scheduledStartTime,
-        LocalDateTime scheduledEndTime
+        List<TeamInfo> teams
 ) {
-    public static TeamListRes fromEntity(Team team, Meeting closestMeeting) {
-        if (closestMeeting != null) {
-            return new TeamListRes(
+    public static TeamListRes from(List<TeamInfo> teamInfos) {
+        return new TeamListRes(teamInfos);
+    }
+
+    public record TeamInfo(
+            Long teamId,
+            String teamName,
+            String meetingName,
+            LocalDateTime scheduledStartTime,
+            LocalDateTime scheduledEndTime
+    ) {
+        public static TeamInfo fromEntity(Team team, Meeting closestMeeting) {
+            if (closestMeeting != null) {
+                return new TeamInfo(
+                        team.getId(),
+                        team.getTeamName().getName(),
+                        closestMeeting.getTitle(),
+                        closestMeeting.getScheduledTime().getScheduledStartTime(),
+                        closestMeeting.getScheduledTime().getScheduledEndTime()
+                );
+            }
+            return new TeamInfo(
                     team.getId(),
                     team.getTeamName().getName(),
-                    closestMeeting.getTitle(),
-                    closestMeeting.getScheduledTime().getScheduledStartTime(),
-                    closestMeeting.getScheduledTime().getScheduledEndTime()
+                    null,
+                    null,
+                    null
             );
         }
-        return new TeamListRes(
-                team.getId(),
-                team.getTeamName().getName(),
-                null,
-                null,
-                null
-        );
     }
 }
 
--- src/main/java/com/jolupbisang/demo/presentation/team/TeamListQueryController.java
+++ src/main/java/com/jolupbisang/demo/presentation/team/TeamListQueryController.java
@@ -9,7 +9,6 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,8 +17,8 @@ public class TeamListQueryController {
     private final TeamListQueryService teamListQueryService;
 
     @GetMapping("/api/v1/teams")
-    public ResponseEntity<List<TeamListRes>> getMyTeams(@AuthenticationPrincipal CustomUserDetails userDetails) {
-        List<TeamListRes> teams = teamListQueryService.getTeamsByUserId(userDetails.getUserId());
+    public ResponseEntity<TeamListRes> getMyTeams(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        TeamListRes teams = teamListQueryService.getTeamsByUserId(userDetails.getUserId());
         
         return ResponseEntity.ok(teams);
     }
