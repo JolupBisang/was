c675dd6f8d871a6a2279fc5aded88bf17542ef75
garden-zero <oneyoung0623@gmail.com>
2025-11-17 18:40:44 +0900

    feat: 팀 삭제 기능 추가

--- src/main/java/com/jolupbisang/demo/infrastructure/team/TeamRepositoryCustom.java
+++ src/main/java/com/jolupbisang/demo/infrastructure/team/TeamRepositoryCustom.java
@@ -1,10 +1,13 @@
 package com.jolupbisang.demo.infrastructure.team;
 
 import com.jolupbisang.demo.domain.team.model.Team;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 
 import java.util.Optional;
 
 public interface TeamRepositoryCustom {
     Optional<Team> findByIdWithMembers(Long teamId);
+    Slice<Team> findByNameContainingAndUserId(String name, Long userId, Pageable pageable);
 }
 
--- src/main/java/com/jolupbisang/demo/infrastructure/team/TeamRepositoryImpl.java
+++ src/main/java/com/jolupbisang/demo/infrastructure/team/TeamRepositoryImpl.java
@@ -3,7 +3,11 @@ package com.jolupbisang.demo.infrastructure.team;
 import com.jolupbisang.demo.domain.team.model.Team;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
 
+import java.util.List;
 import java.util.Optional;
 
 import static com.jolupbisang.demo.domain.team.model.QTeam.team;
@@ -23,6 +27,30 @@ public class TeamRepositoryImpl implements TeamRepositoryCustom {
                 .fetchOne();
 
         return Optional.ofNullable(resultTeam);
+    }
+
+    @Override
+    public Slice<Team> findByNameContainingAndUserId(String name, Long userId, Pageable pageable) {
+        int pageSize = pageable.getPageSize();
+        List<Team> teams = queryFactory
+                .selectFrom(team)
+                .innerJoin(team.members, teamMember)
+                .where(
+                        teamMember.userId.eq(userId)
+                                .and(team.teamName.name.containsIgnoreCase(name))
+                )
+                .distinct()
+                .orderBy(team.id.desc())
+                .limit(pageSize + 1)
+                .offset(pageable.getOffset())
+                .fetch();
+
+        boolean hasNext = teams.size() > pageSize;
+        if (hasNext) {
+            teams.remove(teams.size() - 1);
+        }
+
+        return new SliceImpl<>(teams, pageable, hasNext);
     }
 }
 
