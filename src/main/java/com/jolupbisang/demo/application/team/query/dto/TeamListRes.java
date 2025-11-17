ec9124c2de1088bf38e5d6d26511d603e23210d7
garden-zero <oneyoung0623@gmail.com>
2025-11-17 19:34:17 +0900

    feat: 팀 목록 검색 기능 추가

--- src/main/java/com/jolupbisang/demo/application/team/query/dto/TeamListRes.java
+++ src/main/java/com/jolupbisang/demo/application/team/query/dto/TeamListRes.java
@@ -2,15 +2,17 @@ package com.jolupbisang.demo.application.team.query.dto;
 
 import com.jolupbisang.demo.domain.meeting.model.Meeting;
 import com.jolupbisang.demo.domain.team.model.Team;
+import org.springframework.data.domain.Slice;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 public record TeamListRes(
-        List<TeamInfo> teams
+        List<TeamInfo> teams,
+        boolean hasNext
 ) {
-    public static TeamListRes from(List<TeamInfo> teamInfos) {
-        return new TeamListRes(teamInfos);
+    public static TeamListRes from(Slice<TeamInfo> teamSlice) {
+        return new TeamListRes(teamSlice.getContent(), teamSlice.hasNext());
     }
 
     public record TeamInfo(
